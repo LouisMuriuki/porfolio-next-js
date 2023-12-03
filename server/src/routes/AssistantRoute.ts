c634e0d9c424bae4bc8ba380fca4124eb189fe11
louis hugo <luihugo247@gmail.com>
2023-12-03 12:21:19 +0300

    isolated backend

--- server/src/routes/AssistantRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { startConversation } from "../controller/AssistantController";
-
-const router = express.Router();
-
-router.post("/chat", startConversation);
-
-export default router;
