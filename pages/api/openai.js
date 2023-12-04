79d8b24386669cf137c41bb6d6f03116de6e7a5a
louis hugo <luihugo247@gmail.com>
2023-12-04 06:38:27 +0300

    update

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -26,12 +26,12 @@ export default async function (req, res) {
             incomingmessages.push({ role: role, message: content });
           });
           return res.status(200).json(...incomingmessages);
-        } else {
+        }
           // res.status(400).json({
           //   role: "assistant",
           //   message: "Please try again i did not get that correctly",
           // });
-        }
+        
       });
   } catch (error) {
     console.error("Error:", error);
